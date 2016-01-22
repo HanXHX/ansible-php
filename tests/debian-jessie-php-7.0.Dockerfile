2fa6bc284678ddd90251415e881f7ac46430a7c9
Emilien Mantel <emilien.mantel@debianiste.org>
2016-01-22 16:29:43 +0100

    Travis maanges now PHP version

--- tests/debian-jessie-php-7.0.Dockerfile
+++ tests/debian-jessie-php-7.0.Dockerfile
@@ -1,6 +1,5 @@
 FROM williamyeh/ansible:debian8-onbuild
 
-RUN apt-get update
-RUN ansible-galaxy install HanXHX.dotdeb
-CMD ["sh", "tests/test.sh"]
+RUN apt-get update && ansible-galaxy install HanXHX.dotdeb
+CMD ["sh", "tests/test.sh", "7.0"]
 
--- tests/test.sh
+++ tests/test.sh
@@ -5,17 +5,27 @@
 DIR=$( dirname $0 )
 INVENTORY_FILE="$DIR/inventory"
 PLAYBOOK="$DIR/test.yml"
+ANSIBLE_ARG=""
 
 set -ev
+
+# Only for travis
+if [ -n "$1" ]
+then
+	ANSIBLE_ARG="--extra-vars 'php_version=$1'"
+fi
+
+
+ansible --version
 
 # Check syntax
 ansible-playbook -i $INVENTORY_FILE -c local --syntax-check -vv $PLAYBOOK
 
 # Check role
-ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK
+ansible-playbook -i $INVENTORY_FILE -c local $ANSIBLE_ARG --become -vv $PLAYBOOK
 
 # Check indempotence
-ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK \
+ansible-playbook -i $INVENTORY_FILE -c local $ANSIBLE_ARG --become -vv $PLAYBOOK \
 | grep -q 'changed=0.*failed=0' \
 && (echo 'Idempotence test: pass' && exit 0) \
 || (echo 'Idempotence test: fail' && exit 1)
